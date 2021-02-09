8dd9a0e20f406907ef98ad45d9b3cdfb19e480de
Marcusbelo <78099113+MarcusbeloV2@users.noreply.github.com>
2021-02-08 23:41:25 -0300

    Delete Ex10--Binary.py

--- Ex10--Binary.py
+++
@@ -1,6 +0,0 @@
-#Write a program that asks for an integer and then prints it in binary format
-x=int(input("please print an number so I´ll put in binary form"))
-if x>0:
- print(bin(x)[2:])
-if x<0:
- print( " -",bin(x)[3:])
