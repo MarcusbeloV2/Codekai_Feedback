d663f70d7b2c2bc482b9a7799ab68c854ade80be
Marcusbelo <78099113+MarcusbeloV2@users.noreply.github.com>
2021-02-10 14:55:06 -0300

    Delete Ex4--Sum.py

--- Ex4--Sum.py
+++
@@ -1,7 +0,0 @@
-#Write a program that asks the user for a number n and prints the sum of the numbers 1 to n.
-x=input("please write a number")
-x=int(x)
-if x==0:
- print("0 shouldn´t be used in this operation")
-else:
- print((x + 1) * x /2)
