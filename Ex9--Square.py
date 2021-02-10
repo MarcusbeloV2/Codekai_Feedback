428d476831bfa4889f7c08e025b49b1a1ced2e5a
Marcusbelo <78099113+MarcusbeloV2@users.noreply.github.com>
2021-02-10 14:55:42 -0300

    Delete Ex9--Square.py

--- Ex9--Square.py
+++
@@ -1,8 +0,0 @@
-#Write a program that generates a square of random size. Sizes must be between 1 and 10 (included).
-import random
-a=random.randint(0,9)
-b=0
-c='*'
-while b<a + 1:
- b=b+1
- print(c + c*a)
